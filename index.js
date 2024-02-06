bf80fa74a97215f4f4c40f550237b336c8f9b963
Steven Lai <tinleaves.0@gmail.com>
2024-02-05 19:10:18 -0800

    connected to MySQL and bug fixes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("./utils.js");
+require('./utils');
 
 require('dotenv').config();
 const express = require('express');
@@ -7,217 +7,187 @@ const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
 
-const port = process.env.PORT || 3020;
+
+const database = include('databaseConnection');
+const db_utils = include('database/db_utils');
+const db_users = include('database/users');
+const success = db_utils.printMySQLVersion();
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 
-const Joi = require("joi");
+const expireTime = 1 * 60 * 60 * 1000; 
 
-app.use('/public', express.static('public'));
-
-app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + "/public"));
-
-const expireTime = 1 * 60 * 60 * 1000; //expires after 1 hour  (hours * minutes * seconds * millis)
 
 /* secret information section */
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
-const mongodb_database = process.env.MONGODB_DATABASE;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
-const { database } = require('./databaseConnection');
+app.use('/public', express.static('public'));
 
+app.set('view engine', 'ejs');
 
-const userCollection = database.db(mongodb_database).collection('users');
-
-const { MongoClient, ObjectId } = require('mongodb');
-
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 var mongoStore = MongoStore.create({
-  mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
-  crypto: {
-    secret: mongodb_session_secret
-  }
-});
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+})
 
-app.use(session({
-  secret: node_session_secret,
-  store: mongoStore,
-  saveUninitialized: false,
-  resave: true
+app.use(session({ 
+    secret: node_session_secret,
+	store: mongoStore,
+	saveUninitialized: false, 
+	resave: true
 }
 ));
 
-// function isValidSession(req) {
-//   if (req.session.authenticated) {
-//     return true;
-//   }
-//   return false;
-// }
-
-// function sessionValidation(req, res, next) {
-//   if (isValidSession(req)) {
-//     next();
-//   }
-//   else {
-//     res.redirect('/login');
-//   }
-// }
-
 app.get('/', (req, res) => {
-  const authenticated = req.session.authenticated;
-  const username = req.session.username;
-
-  res.render('index', { authenticated, username });
+    const authenticated = req.session.authenticated;
+    const username = req.session.username;
+  
+    res.render('index', { authenticated, username });
 });
 
 app.get('/members', (req, res) => {
-  if (!req.session.authenticated) {
-    res.redirect('/login');
-    return;
-  }
+    if (!req.session.authenticated) {
+      res.redirect('/login');
+      return;
+    }
+  
+    const images = ['business.gif', 'atVibe.gif', 'atSoupMe.gif'];
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = '/public/' + images[randomIndex];
+  
+    res.render('members', { authenticated: req.session.authenticated, username: req.session.username, randomImage });
+  });
 
-  const images = ['atSad.gif', 'atVibe.gif', 'atSoupMe.gif'];
-  const randomIndex = Math.floor(Math.random() * images.length);
-  const randomImage = '/public/' + images[randomIndex];
+app.get('/createTables', async (req,res) => {
 
-  res.render('members', { authenticated: req.session.authenticated, username: req.session.username, randomImage });
-});
+    const create_tables = include('database/create_tables');
 
-
-app.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.redirect('/');
+    var success = create_tables.createTables();
+    if (success) {
+        res.render("successMessage", {message: "Created tables."} );
+    }
+    else {
+        res.render("errorMessage", {error: "Failed to create tables."} );
+    }
 });
 
 app.get('/signup', (req, res) => {
-  const errorMsg = req.query.error;
-  res.render('signup', { errorMsg });
-});
-
-app.post('/signupSubmit', async (req, res) => {
-  var username = req.body.username;
-  var email = req.body.email;
-  var password = req.body.password;
-
-  // Check for empty fields
-  let errorMsg = "";
-  if (!username) {
-    errorMsg += "Please provide a username. ";
-  }
-  if (!email) {
-    errorMsg += "Please provide an email. ";
-  }
-  if (!password) {
-    errorMsg += "Please provide a password. ";
-  }
-
-  // If any field is missing, redirect back to /signup with error message
-  if (errorMsg !== "") {
-    res.redirect(`/signup?error=${encodeURIComponent(errorMsg)}`);
-    return;
-  }
-
-  // Validate inputs using Joi
-  const schema = Joi.object({
-    username: Joi.string().alphanum().max(20).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().max(20).required()
+    const errorMsg = req.query.error;
+    res.render('signup', { errorMsg });
   });
 
-  const validationResult = schema.validate({ username, email, password });
-  if (validationResult.error != null) {
-    console.log(validationResult.error);
-    res.redirect(`/signup?error=${encodeURIComponent("Invalid input. Please check your details.")}`);
-    return;
-  }
+app.post('/signupSubmit', async (req,res) => {
+    var username = req.body.username;
+    var password = req.body.password;
 
-  // Check if username already exists
-  const existingUser = await userCollection.findOne({ username: { $eq: username } });
-  if (existingUser) {
-    const errorMsg = "Username already exists.";
-    res.redirect(`/signup?error=${encodeURIComponent(errorMsg)}`);
-    return;
-  }
+    let errorMsg = "";
+    if (!username) {
+      errorMsg += "Please provide a username. ";
+    }
+    if (!password) {
+      errorMsg += "Please provide a password. ";
+    }
+  
+    if (errorMsg !== "") {
+      res.redirect(`/signup?error=${encodeURIComponent(errorMsg)}`);
+      return;
+    }
 
-  var hashedPassword = await bcrypt.hash(password, saltRounds);
+    var hashedPassword = bcrypt.hashSync(password, saltRounds);
 
-  // Add user to MongoDB database using parameterized query
-  await userCollection.insertOne({
-    username,
-    email,
-    password: hashedPassword,
-    user_type: "user", // Assigning a default user_type of "user"
-  });
+    var success = await db_users.createUser({ user: username, hashedPassword: hashedPassword });
 
-  console.log("Inserted user");
+    if (success) {
+        var results = await db_users.getUsers();
 
-  // Set session variables
-  req.session.authenticated = true;
-  req.session.username = username;
-  req.session.email = email;
-  req.session.user_type = "user";
-
-  res.redirect("/members");
+        res.redirect("/members");
+    }
 });
 
 app.get('/login', (req, res) => {
-  res.render('login');
-});
+    const loginMsg = req.query.error;
+    res.render('login', { loginMsg });
+  });
 
 app.post('/loginSubmit', async (req, res) => {
-  // extract email and password from request body
-  const { email, password } = req.body;
+    const username = req.body.username;
+    const password = req.body.password;
 
-  // validate email using Joi
-  const emailSchema = Joi.string().email().required();
-  const emailValidationResult = emailSchema.validate(email);
-  if (emailValidationResult.error != null) {
-    console.log(emailValidationResult.error);
-    res.redirect('/login');
-    return;
-  }
+    let loginMsg = "";
 
-  // find user in database using email
-  const user = await userCollection.findOne({ email });
-  if (!user) {
-    console.log('invalid email/password combination');
-    const errorMsg = 'Invalid email/password combination.';
-    res.render('loginSubmit', { errorMsg: errorMsg });
-    return;
-  }
+    const results = await db_users.getUser({ user: username });
 
-  // compare password with stored BCrypted password
-  const isPasswordMatch = await bcrypt.compare(password, user.password);
-  if (!isPasswordMatch) {
-    console.log('password is incorrect');
-    const errorMsg = 'Password is incorrect.';
-    res.render('loginSubmit', { errorMsg: errorMsg });
-    return;
-  }
+    if (!username || results.length !== 1) {
+        loginMsg = "User not found. ";
+        res.redirect(`/login?error=${encodeURIComponent(loginMsg)}`);
+        return;
+    }
 
-  // store username in session
-  req.session.authenticated = true;
-  req.session.username = user.username;
-  req.session.cookie.maxAge = expireTime;
-  req.session.user_type = user.user_type;
+    if (!password) {
+        loginMsg = "Incorrect password. Please try again. ";
+        res.redirect(`/login?error=${encodeURIComponent(loginMsg)}`);
+        return;
+    }
 
-  // redirect to members page
-  res.redirect('/members');
+    const storedPassword = results[0].password;
+    if (bcrypt.compareSync(password, storedPassword)) {
+        req.session.authenticated = true;
+        req.session.username = username;
+        req.session.cookie.maxAge = expireTime;
+
+        res.redirect('/members');
+        return;
+    } else {
+        // Incorrect password
+        loginMsg = "Incorrect password. Please try again. ";
+        res.redirect(`/login?error=${encodeURIComponent(loginMsg)}`);
+        return;
+    }
 });
 
-app.get("*", (req, res) => {
-  res.status(404);
-  res.render("404");
-});
+  
+function isValidSession(req) {
+	if (req.session.authenticated) {
+		return true;
+	}
+	return false;
+}
+
+function sessionValidation(req, res, next) {
+	if (!isValidSession(req)) {
+		req.session.destroy();
+		res.redirect('/login');
+		return;
+	}
+	else {
+		next();
+	}
+}
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+  });
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req,res) => {
+	res.status(404);
+	res.render("404");
+})
 
 app.listen(port, () => {
-  console.log("Node application listening on port " + port);
-});
+	console.log("Node application listening on port "+port);
+}); 
